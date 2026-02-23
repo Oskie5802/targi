5d16e84de1b76e06c80da1b108d53bb31c37f1b0
oskie5802 <minoroskar2009@gmail.com>
2026-02-23 12:45:37 +0100

    ip

--- Ptak/start_game.bat
+++ Ptak/start_game.bat
@@ -8,6 +8,6 @@ echo.
 echo ========================================
 echo URUCHAMIANIE SERWERA GRY...
 echo ========================================
-start https://192.168.0.110:5001
+start https://10.10.10.10:5001
 python server.py
 pause
